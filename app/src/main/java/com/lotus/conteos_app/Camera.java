1306b01659bd7c2933eb2a861aa10e815663c977
Amarelle <ewige.qual@gmail.com>
2019-08-21 09:51:55 -0500

    parcial

--- app/src/main/java/com/lotus/conteos_app/Camera.java
+++ app/src/main/java/com/lotus/conteos_app/Camera.java
@@ -40,33 +40,25 @@ public class Camera extends AppCompatActivity {
                 setContentView(R.layout.activity_main);
                 int bc = Integer.parseInt(result.getContents());
 
-                Toast toast = Toast.makeText(getApplicationContext(), String.valueOf(bc), Toast.LENGTH_SHORT);
-                toast.show();
+                    if (bc != 0) {
+                            Toast toast = Toast.makeText(getApplicationContext(), "si hay resultado " + bc, Toast.LENGTH_SHORT);
+                            toast.show();
+                        vbc.stopCamera();//aqui apaga la camara
+                        int resulcode=bc;
+                        Intent intent= new Intent(Camera.this,MainActivity.class);
+                        intent.putExtra(null,resulcode);
+                        startActivity(intent);
 
-                if (bc != 0) {
-                    toast = Toast.makeText(getApplicationContext(), "si hay resultado " + bc, Toast.LENGTH_SHORT);
-                    toast.show();
+                        vbc.stopCamera();//aqui apaga la camara
 
-                    /*MainActivity ma= new MainActivity();
-                    ma.buscarSiembra();*/
-
-                    vbc.stopCamera();//aqui apaga la camara
-
-                    resulcode = (EditText) findViewById(R.id.resulcode);
-                    resulcode.setText(result.getContents());
-
-
-                } else {
-                    toast = Toast.makeText(getApplicationContext(), "no hay resultado", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-
-
+                    } else {
+                            Toast toast = Toast.makeText(getApplicationContext(), "no hay resultado", Toast.LENGTH_SHORT);
+                            toast.show();
+                    }
             } catch (Exception e) {
                 Log.d("ERROR: ", e.toString());
+                vbc.stopCamera();//aqui apaga la camara
             }
-
-
         }
     }
 
@@ -74,6 +66,46 @@ public class Camera extends AppCompatActivity {
     //PARA VOLVER A LA ACTIVIDAD ANTERIOR(CAMARA)
     public void onBackPressed() {
         //Toast.makeText(this,"se retrocedio",Toast.LENGTH_LONG).show();
+        Intent i = new Intent(Camera.this, MainActivity.class);
+        startActivity(i);
+        vbc.stopCamera();//aqui apaga la camara
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "OnStart camara", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de hacerse visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "OnResume camara", Toast.LENGTH_SHORT).show();
+        // La actividad se ha vuelto visible (ahora se "reanuda").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "OnResume camara", Toast.LENGTH_SHORT).show();
+        // Enfocarse en otra actividad  (esta actividad está a punto de ser "detenida").
+        Intent i = new Intent(Camera.this, MainActivity.class);
+        startActivity(i);
+        vbc.stopCamera();//aqui apaga la camara
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop camara", Toast.LENGTH_SHORT).show();
+        // La actividad ya no es visible (ahora está "detenida")
+        Intent i = new Intent(Camera.this, MainActivity.class);
+        startActivity(i);
+        vbc.stopCamera();//aqui apaga la camara
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy camara", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de ser destruida.
         Intent i = new Intent(Camera.this, MainActivity.class);
         startActivity(i);
         vbc.stopCamera();//aqui apaga la camara
