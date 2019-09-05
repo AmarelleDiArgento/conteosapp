0386cb9df99ccc61d7b1938eb4a4a7b41a385775
Aldair Stiven <alda.sttiven@gmail.com>
2019-09-05 00:49:37 -0500

    Layout de quienes somos y logo en el ejecutable

---
+++ app/src/main/java/com/lotus/conteos_app/quienesomos.java
@@ -0,0 +1,13 @@
+package com.lotus.conteos_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class quienesomos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quienesomos);
+    }
+}
